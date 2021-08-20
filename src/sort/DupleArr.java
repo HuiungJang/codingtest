2149d0371bd79cac8415a5b3a98d87b7626a91fb
HuiungJang <jhw1345@gmail.com>
2021-08-21 00:14:50 +0900

    중복된 값 배열에서 전부 제거하고 오름차순으로 정렬하기 - 완료 - 기준 오른쪽만 생각했음, 기준 왼쪽이랑도 비교해야 같은지 비교가능

--- src/sort/DupleArr.java
+++ src/sort/DupleArr.java
@@ -21,7 +21,8 @@ public class DupleArr {
             ref=arr[i];
             // 기준 다음 숫자부터 끝까지 반복
             for(int j=i+1; j<arr.length; j++){
-                if(arr[i]==arr[j]){ // i 번째와 i+1 번째가 같다면
+                if(arr[i]==arr[j] || arr[i]==arr[j-1]){
+                    // i 번째와 i+1 번째가 같다면 또는 i와 i-1번째가 같다면
                     if(arr[j]==ref ){ // 0으로 변환
                        arr[i]=0;
                        arr[j]=0;
@@ -47,16 +48,21 @@ public class DupleArr {
 
         // 중복됐던 값과 0을 지운 값을 새로운 배열에 담아서 리턴.
         int[] result = new int[tmp.size()];
+
+        // 중복되지 않은 값이 없다면 = 전부 중복이라면
+        if(tmp.size()==0){
+            result[0]=-1;
+        }
+        // 값을 int형 배열에 넣어서 리턴
         for(int i=0; i<tmp.size(); i++){
             result[i] = tmp.get(i);
         }
-
 
         return result;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,1,1,2,5,7,5,5,3,7};
+        int[] arr = {5,1,1,1,2,5,5,7,5,5,3,7};
 
         for(Object i :solution(arr)) {
             System.out.println(i);
