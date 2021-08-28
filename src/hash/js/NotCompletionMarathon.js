58785c4d15d84466c20285f8b1a46d6855c532ac
HuiungJang <jhw1345@gmail.com>
2021-08-29 00:08:44 +0900

    프로그래머스 완주하지 못한 선수 - pop() 이용해서 풀기

--- src/hash/js/NotCompletionMarathon.js
+++ src/hash/js/NotCompletionMarathon.js
@@ -7,18 +7,27 @@
 function solution(participant, completion) {
   participant.sort();
   completion.sort();
+  // for (let i = 0; i < participant.length; i++) {
+  //   try {
+  //     if (participant[i] !== completion[i]) {
+  //       return participant[i];
+  //     }
+  //   } catch (e) {
+  //     return participant[participant.length - 1];
+  //   }
+  // }
 
-  for (let i = 0; i < participant.length; i++) {
-    try {
-      if (participant[i] !== completion[i]) {
-        return participant[i];
-      }
-    } catch (e) {
-      return participant[participant.length - 1];
+  // pop() 이용해서 풀기
+  while (participant.length) {
+    let p = participant.pop();
+
+    if (p !== completion.pop()) {
+      return p;
     }
   }
 }
 
 
-console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]));
+console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]));
+
 
